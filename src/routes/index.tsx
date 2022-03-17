3aadf173552ff1d56116f0e9ce91e9c3d3342b18
Chandara-Sin <66314482+Chandara-Sin@users.noreply.github.com>
2022-03-17 22:41:40 +0700

    refactor: display component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,17 @@
 import { BrowserRouter } from "react-router-dom";
+import { Box } from "@mui/material";
 import Header from "../components/header/Header";
+import MessageInput from "../components/message-input/MessageInput";
 
 function Routes() {
   return (
     <BrowserRouter>
       <Header />
+      <main className="drawer-content-margin">
+        <Box className="container">
+          <MessageInput />
+        </Box>
+      </main>
       {/* <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="about" element={<About />} />
