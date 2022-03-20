0e77319929b3adec51dce5565eb4ecdfd01781d4
Chandara-Sin <66314482+Chandara-Sin@users.noreply.github.com>
2022-03-20 17:35:08 +0700

    feat: add message section view

---
+++ src/view/message-section/index.tsx
@@ -0,0 +1,110 @@
+import React, { useState } from "react";
+import "./MessageSection.css";
+import { Box, Button, Paper, Typography } from "@mui/material";
+import {
+  AddRounded,
+  KeyboardArrowRightRounded,
+  NumbersRounded,
+  PersonRounded,
+} from "@mui/icons-material";
+import TooltipShortcut from "../../components/tooltip-shortcut/TooltipShortcut";
+import ChannelDetailsDialog from "../../components/channel-details-dialog/ChannelDetailsDialog";
+import Message from "../../components/message/Message";
+
+function ChannelDetails() {
+  const [openChannelDetailsDialog, setOpenChannelDetailsDialog] =
+    useState(false);
+
+  const handleOpenChannelDetailDialog = () => {
+    setOpenChannelDetailsDialog(true);
+  };
+
+  const handleCloseChannelDetailsDialog = () => {
+    setOpenChannelDetailsDialog(false);
+  };
+
+  return (
+    <Paper className="channel-details flex-between align-center" elevation={0}>
+      <TooltipShortcut
+        style={{ marginBottom: "-5px" }}
+        hasArrow
+        tooltipContent={{
+          title: {
+            name: "Get channel details",
+            style: { fontSize: "0.8rem" },
+          },
+        }}
+      >
+        <Button
+          variant="text"
+          className="text-transform-none channel-details-button"
+          color="inherit"
+          onClick={handleOpenChannelDetailDialog}
+        >
+          <Box className="content-center">
+            <NumbersRounded className="tooltip-shortcut-button" />
+            <Typography className="text-bold">general</Typography>
+            <KeyboardArrowRightRounded className="tooltip-shortcut-button " />
+          </Box>
+        </Button>
+      </TooltipShortcut>
+      <TooltipShortcut
+        placement="bottom-end"
+        style={{ marginBottom: "-5px" }}
+        hasArrow
+        tooltipContent={{
+          title: {
+            name: "View all members of this channel",
+            style: { fontSize: "0.8rem" },
+          },
+        }}
+      >
+        <Button
+          variant="outlined"
+          className="member-details-button"
+          startIcon={<PersonRounded className="member-details-icon" />}
+        >
+          12
+        </Button>
+      </TooltipShortcut>
+      <ChannelDetailsDialog
+        open={openChannelDetailsDialog}
+        onClose={handleCloseChannelDetailsDialog}
+      />
+    </Paper>
+  );
+}
+
+function ChannelBookmark() {
+  return (
+    <Paper className="channel-bookmark" elevation={0}>
+      <Button
+        variant="text"
+        className="text-transform-none channel-details-button"
+        color="inherit"
+      >
+        <Box className="content-center">
+          <AddRounded className="tooltip-shortcut-button text-grey-4" />
+          <Typography variant="subtitle2" className="text-grey-4">
+            Add a bookmark
+          </Typography>
+        </Box>
+      </Button>
+    </Paper>
+  );
+}
+
+function MessageSection() {
+  return (
+    <>
+      <ChannelDetails />
+      <ChannelBookmark />
+      <Box className="message-display-card fill-height">
+        <Message />
+        <Box sx={{ mt: "150px" }}></Box>
+      </Box>
+    </>
+  );
+}
+
+export default MessageSection;
