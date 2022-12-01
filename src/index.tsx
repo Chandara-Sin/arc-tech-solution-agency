7f6259f70212363700cd1b896f0b57fb9079a4e3
Chandara Sin <chandara388588@gmail.com>
2022-12-01 11:32:08 +0700

    perf: remove unused dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,10 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { store } from "./app/store";
-import { Provider } from "react-redux";
 
 const root = createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>
 );
