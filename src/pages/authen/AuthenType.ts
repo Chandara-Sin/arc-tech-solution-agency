9dde4fd84efcf732e8fc461fcd88b4275e6ecbc8
Chandara-Sin <cdsin68@gmail.com>
2023-04-22 21:49:17 +0700

    feat: add auth code input

--- src/pages/authen/AuthenType.ts
+++ src/pages/authen/AuthenType.ts
@@ -1,3 +1,13 @@
 export interface IFormSignIn {
   email: string;
 }
+
+export interface AuthCodeRef {
+  focus: () => void;
+  clear: () => void;
+}
+
+export interface AuthCodeProps {
+  onChange?: (value: string) => void;
+  length?: number;
+}
--- src/pages/authen/VerifyCode.tsx
+++ src/pages/authen/VerifyCode.tsx
@@ -1,98 +1,111 @@
 import { Divider, Link, TextField, Typography } from "@mui/material";
-import "./Authen.css";
 import { Box, Container } from "@mui/system";
+import { forwardRef, Fragment, useImperativeHandle, useRef } from "react";
 import GmailLogo from "../../assets/icon/gmail-logo";
 import OutlookLogo from "../../assets/icon/outlook-logo";
+import "./Authen.css";
+import { AuthCodeProps, AuthCodeRef } from "./AuthenType";
 
-const VerifyCode = () => (
-  <Container className="signin-container d-flex flex-column align-center">
-    <Typography className="text-bold mb-2" variant="h3">
-      Check your email for a code
-    </Typography>
-    <Typography className="sub-heading text-center" variant="subtitle1">
-      We've send a 6-character code to <strong>example@gmail.com</strong>. The
-      code expired shortly, so please enter it soon.
-    </Typography>
-    <Box className="signin-started align-center text-center justify-center full-width">
+const AuthCode = forwardRef<AuthCodeRef, AuthCodeProps>(
+  ({ onChange, length = 6 }) => {
+    const inputsRef = useRef<Array<HTMLInputElement>>([]);
+
+    const handleOnChange = (
+      e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+      index: number
+    ) => {
+      const { value } = e.target;
+      const nextElementSlibing = ++index;
+      if (value.match(/\w{1}/)) {
+        inputsRef.current[nextElementSlibing] &&
+          inputsRef.current[nextElementSlibing].focus();
+      } else {
+        e.target.value = "";
+      }
+      handleOnSubmit();
+    };
+
+    const handleOnSubmit = () => {
+      const value = inputsRef.current.map(({ value }) => value).join("");
+      onChange && onChange(value);
+    };
+
+    const handleOnFocus = (e: React.FocusEvent<HTMLInputElement>) =>
+      e.target.select();
+
+    return (
       <form
         autoComplete="off"
         className="d-flex align-center"
         style={{ paddingBottom: "50px" }}
       >
-        <TextField
-          type="text"
-          variant="outlined"
-          size="small"
-          inputProps={{ maxLength: "1" }}
-          className="verify-input"
-        />
-        <TextField
-          type="text"
-          variant="outlined"
-          size="small"
-          inputProps={{ maxLength: "1" }}
-          className="verify-input"
-        />
-        <TextField
-          type="text"
-          variant="outlined"
-          size="small"
-          inputProps={{ maxLength: "1" }}
-          className="verify-input"
-        />
-        <Divider
-          className="mx-2"
-          style={{ width: "10px", borderColor: "black" }}
-        />
-        <TextField
-          type="text"
-          variant="outlined"
-          size="small"
-          inputProps={{ maxLength: "1" }}
-          className="verify-input"
-        />
-        <TextField
-          type="text"
-          variant="outlined"
-          size="small"
-          inputProps={{ maxLength: "1" }}
-          className="verify-input"
-        />
-        <TextField
-          type="text"
-          variant="outlined"
-          size="small"
-          inputProps={{ maxLength: "1" }}
-          className="verify-input"
-        />
+        {[...Array(length)].map((_, i) => (
+          <Fragment key={i}>
+            <TextField
+              type="text"
+              variant="outlined"
+              size="small"
+              inputProps={{ maxLength: "1" }}
+              className="verify-input"
+              onChange={(e) => handleOnChange(e, i)}
+              onFocus={handleOnFocus}
+              inputRef={(el: HTMLInputElement) => {
+                inputsRef.current[i] = el;
+              }}
+            />
+            {i === 2 && (
+              <Divider
+                className="mx-2"
+                style={{ width: "10px", borderColor: "black" }}
+              />
+            )}
+          </Fragment>
+        ))}
       </form>
-      <Box className="d-flex align-center justify-space-evenly my-4">
-        <Link
-          className="text-transform-capitalize d-flex align-center pointer"
-          underline="hover"
-        >
-          <GmailLogo
-            className="pr-1"
-            style={{ width: "1rem", height: "1rem" }}
-          />
-          Open Gmail
-        </Link>
-        <Link
-          className="text-transform-capitalize d-flex align-center pointer"
-          underline="hover"
-        >
-          <OutlookLogo
-            className="pr-1"
-            style={{ width: "1.6rem", height: "1.6rem" }}
-          />
-          Open OutLook
-        </Link>
-      </Box>
-      <Typography className="signin-manual-text">
-        Can't find your code? Check your spam folder!
-      </Typography>
-    </Box>
-  </Container>
+    );
+  }
 );
+
+const VerifyCode = () => {
+  return (
+    <Container className="signin-container d-flex flex-column align-center">
+      <Typography className="text-bold mb-2" variant="h3">
+        Check your email for a code
+      </Typography>
+      <Typography className="sub-heading text-center" variant="subtitle1">
+        We've send a 6-character code to <strong>example@gmail.com</strong>. The
+        code expired shortly, so please enter it soon.
+      </Typography>
+      <Box className="signin-started align-center text-center justify-center full-width">
+        <AuthCode />
+        <Box className="d-flex align-center justify-space-evenly my-4">
+          <Link
+            className="text-transform-capitalize d-flex align-center pointer"
+            underline="hover"
+          >
+            <GmailLogo
+              className="pr-1"
+              style={{ width: "1rem", height: "1rem" }}
+            />
+            Open Gmail
+          </Link>
+          <Link
+            className="text-transform-capitalize d-flex align-center pointer"
+            underline="hover"
+          >
+            <OutlookLogo
+              className="pr-1"
+              style={{ width: "1.6rem", height: "1.6rem" }}
+            />
+            Open OutLook
+          </Link>
+        </Box>
+        <Typography className="signin-manual-text">
+          Can't find your code? Check your spam folder!
+        </Typography>
+      </Box>
+    </Container>
+  );
+};
 
 export default VerifyCode;
